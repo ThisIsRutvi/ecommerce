d84f16da612f9f5a9a3debeccf33f3add838150b
Rutvi Dave <130861211+ThisIsRutvi@users.noreply.github.com>
2025-01-07 20:23:04 +0530

    Delete backend/router/router.js

--- backend/router/router.js
+++
@@ -1,5 +0,0 @@
-/*import express from 'express';
-const router = express.Router();
-import { addPaymentGateway } from '../controller/payment-controller';
-
-router.post('/payment',addPaymentGateway);*/
